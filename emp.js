6379eeeaeb3503b6e72865a99f76f304f0906134
SugataMaji <sugatamaji94@gmail.com>
2017-12-03 17:24:46 +0530

    Add files via upload

---
+++ emp.js
@@ -0,0 +1,9 @@
+var employee={
+ address:{
+ EmployeeId:1001,
+ EmployeeName: "Sugata Maji",
+ job:"Developer"
+ }
+};
+employee.address.EmployeeName;
+console.log("Employee ID: "+employee.address.EmployeeId,", Employee Name: "+employee.address.EmployeeName, ", Employee Job: "+employee.address.job);
---
+++ sort.js
@@ -0,0 +1,7 @@
+var numbers=[3,4,5,6,67,8];
+console.log(numbers.sort());
+numbers.unshift(14);
+console.log(numbers);
+numbers.splice(1, 0, "21");
+numbers.splice(2, 0, "26");
+console.log(numbers);
